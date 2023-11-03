a158c5de3e95378c80c9b9d3bb7fc3f253e2c73f
luhyxi <luhyxi@gmail.com>
2023-11-02 22:08:21 -0300

    Started quicksort implementation

---
+++ quicksort.c
@@ -0,0 +1,14 @@
+//
+// Created by luhyxi on 02/11/23.
+//
+
+#include <stdlib.h>
+#include <time.h>
+
+void swap(int *x, int *y) {
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+// TODO IMPLEMENTAR O RESTO!!!!!!!!!!
---
+++ quicksort.h
@@ -0,0 +1,12 @@
+//
+// Created by luhyxi on 02/11/23.
+//
+
+#ifndef LEETGOONING_QUICKSORT_H
+#define LEETGOONING_QUICKSORT_H
+
+void swap(int *x, int *y);
+void partition(int array[], int low, int high);
+void quicksort(int array[], int length);
+void quicksortRecursion(int array[], int low, int high);
+#endif //LEETGOONING_QUICKSORT_H
